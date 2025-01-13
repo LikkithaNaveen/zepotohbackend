39deaededd630596a9ce513bd826c621d80c4b6d
Likkitha <likkithalikis01@gmail.com>
2025-01-13 13:32:20 +0530

    changed

--- index.js
+++ index.js
@@ -38,15 +38,15 @@ app.delete('/getUser-Details/:id',async(req,res)=>{
 }
 })
 
-// app.get('/updategetUser-Details/:id',async(req,res)=>{
-//   let result= await user.findOne({_id:req.params.id});
-//     if(result){
-//       res.send(result)
-//     }else{
-//       res.send({result:'No record found'})
-//   }
-//   res.send(result)
-// });
+app.get('/updategetUser-Details/:id',async(req,res)=>{
+  let result= await user.findOne({_id:req.params.id});
+    if(result){
+      res.send(result)
+    }else{
+      res.send({result:'No record found'})
+  }
+  res.send(result)
+});
 
 app.put('/updategetUser-Details/:id',async(req,res)=>{
   let result= await user.updateOne({_id:req.params.id},

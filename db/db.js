e773d0d53e7d8a82f9ba97a7478bcaa51a537881
Likkitha <likkithalikis01@gmail.com>
2025-01-13 10:29:56 +0530

    changes

--- db/db.js
+++ db/db.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/contactsDB');
+mongoose.connect('mongodb+srv://lamadev:8jrqyCXOkW7uWEtw@cluster0.pbqp57k.mongodb.net/contactsDB');
